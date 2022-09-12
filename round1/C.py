af0643b54a77a71446c8289a0acd223e9bbac2f0
Xinyi Li <wolixinyi@gmail.com>
2022-09-12 07:15:24 +0000

    C problem

--- round1/C.py
+++ round1/C.py
@@ -17,32 +17,31 @@ def convex_hull(points):
         while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
             upper.pop()
         upper.append(p)
-    return [points[0]] + sorted(lower[1:-1] + upper[1:-1]) + [points[-1]]
+    return [points[0]] + lower[1:-1] + upper[1:-1] + [points[-1]]
 
 def dist(a,b):
     return (a[0]-b[0])**2 + (a[1]-b[1])**2
 
-def solve(N,K,D,points):
+def solve(N, K, D,points):
     hull = convex_hull(points)
     n = len(hull)
 
-    dp = {0:0}
-    seen = set()
+    dp = {i:float('inf') for i in range(n)}
+    dp[0] = 0
 
     while dp:
         idx = min(dp.keys(), key=dp.__getitem__)
-        seen.add(idx)
+
+        cur = dp[idx]
+        if cur == float('inf'):
+            return -1
         if idx == n-1:
             return dp[idx]
-        for nei in range(idx+1,n):
-            if hull[nei][0] - hull[idx][0] > D:
-                break
-            d = dist(hull[idx],hull[nei])
-            delta = max(K,d)
-            if d > D**2: continue
-            if nei not in seen and (nei not in dp or dp[nei] > dp[idx] + delta):
-                dp[nei] = dp[idx] + delta
         del dp[idx]
+        for i in dp:
+            d = dist(hull[idx], hull[i])
+            if d <= D**2:
+                dp[i] = min(dp[i], cur + max(K, d))
     return -1
 
 
@@ -52,5 +51,4 @@ T = int(input())
 for t in range(1, T + 1):
     N, K, D = map(int,input().split())
     points = [tuple(map(int, input().split())) for _ in range(N)]
-    # if t!=2: continue
     print(f"Case #{t}: {solve(N,K,D,points)}")
