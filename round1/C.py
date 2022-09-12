cbf2feccde1bfe77845651a69c252cefaecfcefb
Xinyi Li <wolixinyi@gmail.com>
2022-09-12 07:26:33 +0000

    update C

--- round1/C.py
+++ round1/C.py
@@ -1,8 +1,6 @@
-import heapq
-from functools import lru_cache
-
 def cross(o, a, b):
     return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
 
 def convex_hull(points):
     points.sort()
@@ -11,7 +9,7 @@ def convex_hull(points):
         while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
             lower.pop()
         lower.append(p)
-    
+
     upper = []
     for p in reversed(points):
         while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
@@ -19,14 +17,16 @@ def convex_hull(points):
         upper.append(p)
     return [points[0]] + lower[1:-1] + upper[1:-1] + [points[-1]]
 
-def dist(a,b):
+
+def dist(a, b):
     return (a[0]-b[0])**2 + (a[1]-b[1])**2
 
-def solve(N, K, D,points):
+
+def solve(N, K, D, points):
     hull = convex_hull(points)
     n = len(hull)
 
-    dp = {i:float('inf') for i in range(n)}
+    dp = {i: float('inf') for i in range(n)}
     dp[0] = 0
 
     while dp:
@@ -45,10 +45,8 @@ def solve(N, K, D,points):
     return -1
 
 
-
-
 T = int(input())
 for t in range(1, T + 1):
-    N, K, D = map(int,input().split())
+    N, K, D = map(int, input().split())
     points = [tuple(map(int, input().split())) for _ in range(N)]
-    print(f"Case #{t}: {solve(N,K,D,points)}")
+    print(f"Case #{t}: {solve(N, K, D, points)}")
